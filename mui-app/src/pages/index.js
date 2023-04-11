3ad9c5dd7bebc40508943d291b602c168ddcef24
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2023-04-11 12:23:00 -0400

    adding project struture

---
+++ mui-app/src/pages/index.js
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { Typography } from '@mui/material'
+
+
+export default function Home() {
+  return (
+    <>
+     <Typography variant='h1'>hello world</Typography>
+    </>
+  )
+}
